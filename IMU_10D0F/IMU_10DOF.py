216af1d33cab1bc0050a78924648a7e8134e0d2a
Flavio Henrique Madureira Bergamini <flaviobergamini@gec.inatel.br>
2020-05-17 13:27:31 -0300

    Alteração para leitura dos dados do sensor via serial

--- IMU_10D0F/IMU_10DOF.py
+++ IMU_10D0F/IMU_10DOF.py
@@ -1,108 +1,36 @@
-import smbus
+import serial
 import time
-import math
-
-bus = smbus.SMBus(1)
-
-# Acesso aos endereços do MPU9250
-
-MPU9250_ADDRESS_AD0_LOW = 0x68
-MPU9250_DEFAULT_ADDRESS = MPU9250_ADDRESS_AD0_LOW
-MPU9250_RA_INT_PIN_CFG = 0x37
-
-# Acesso aos endereços do magnetômetro do MPU9250
-MPU9150_RA_MAG_ADDRESS = 0x0C
-MPU9150_RA_MAG_XOUT_L = 0x03
-MPU9150_RA_MAG_YOUT_L = 0x05
-MPU9150_RA_MAG_ZOUT_L = 0x07
-
-# Acesso aos endereços do acelerômetro do MPU9250
-MPU9250_RA_ACCEL_XOUT_H = 0x3B
-MPU9250_RA_ACCEL_XOUT_L = 0x3C
-MPU9250_RA_ACCEL_YOUT_H = 0x3D
-MPU9250_RA_ACCEL_YOUT_L = 0x3E
-MPU9250_RA_ACCEL_ZOUT_H = 0x3F
-MPU9250_RA_ACCEL_ZOUT_L = 0x40
-
-# Acesso aos endereços do giroscópio do MPU9250
-MPU9250_RA_GYRO_CONFIG = 0x1B
-MPU9250_RA_GYRO_XOUT_H = 0x43
-MPU9250_RA_GYRO_XOUT_L = 0x44
-MPU9250_RA_GYRO_YOUT_H = 0x45
-MPU9250_RA_GYRO_YOUT_L = 0x46
-MPU9250_RA_GYRO_ZOUT_H = 0x47
-MPU9250_RA_GYRO_ZOUT_L = 0x48
-
-
 
 class IMU10DOF(object):
-        def __init__(self):
-                pass
+        def __init__(self, port, baudrate):                                   #Recebe os dados para realizar um conexão serial
+            self.COMserial = serial.Serial(port, baudrate=baudrate)
 
         def read_magnetometer(self):
-                global mx
-                global my
-                global mz
-                bus.write_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_INT_PIN_CFG, 0x02)     # habilitar leituras
-                time.sleep(0.1)
-                bus.write_byte_data(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01)     # ativa o magnetômetro
-                time.sleep(0.1)
-                mx = bus.read_byte_data(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L) # busca o valor no eixo x
-                my = bus.read_byte_data(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_YOUT_L) # busca o valor no eixo y
-                mz = bus.read_byte_data(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_ZOUT_L) # busca o valor no eixo z
-                mx = mx * 1200 / 4096
-                my = my * 1200 / 4096
-                mz = mz * 1200 / 4096
-                self.getHeading()
-                return mx, my, mz
+            time.sleep(2)
+            self.COMserial.write('m'.encode())
+            mag = self.COMserial.readline()
+            return mag
 
         def read_accelerometer(self):
-                global ax
-                global ay
-                global az
-                bus.write_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_INT_PIN_CFG, 0x02) # habilitar leituras
-                time.sleep(0.1)
-                ax = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_ACCEL_XOUT_H) # busca o valor no eixo x
-                ay = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_ACCEL_YOUT_H) # busca o valor no eixo y
-                az = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_ACCEL_ZOUT_H) # busca o valor no eixo z
-                ax = ax << 8 | ax
-                ay = ay << 8 | ay
-                az = az << 8 | az
-                ax = ax / 16384
-                ay = ay / 16384
-                az = az / 16384
-                return ax, ay, az
+            time.sleep(2)
+            self.COMserial.write('a'.encode())
+            acc = self.COMserial.readline()
+            return acc
 
         def read_gyroscope(self):
-                bus.write_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_INT_PIN_CFG, 0x02) # habilitar leituras
-                time.sleep(0.1)
-                gx = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_GYRO_XOUT_H) # busca o valor no eixo x
-                gy = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_GYRO_YOUT_H) # busca o valor no eixo y
-                gz = bus.read_byte_data(MPU9250_DEFAULT_ADDRESS, MPU9250_RA_GYRO_ZOUT_H) # busca o valor no eixo z
-                gx = gx << 8 | gx
-                gy = gy << 8 | gy
-                gz = gz << 8 | gz
-                gx = gx * 250 / 32768
-                gy = gy * 250 / 32768
-                gz = gz * 250 / 32768
-                return gx, gy, gz
+            time.sleep(2)
+            self.COMserial.write('g'.encode())
+            gyr = self.COMserial.readline()
+            return gyr
 
-        def getHeading(self):
-                heading = 180 * math.atan2(my, mx) / math.pi
-                if heading < 0 :
-                        heading += 360
-                return heading
+        def get_heading(self):
+            time.sleep(2)
+            self.COMserial.write('x'.encode())
+            hea = self.COMserial.readline()
+            return hea
 
-        def getTiltHeading(self):
-                global tiltheading
-                pitch = math.asin(-ax)
-                roll = math.asin(ay / math.cos(pitch))
-
-                xh = mx * math.cos(pitch) + mz * math.sin(pitch)
-                yh = mx * math.sin(roll) * math.sin(pitch) + my * math.cos(roll) - mz * math.sin(roll) * math.cos(pitch)
-                zh = -mx * math.cos(roll) * math.sin(pitch) + my * math.sin(roll) + mz * math.cos(roll) * math.cos(pitch)
-                tiltheading = 180 * math.atan2(yh, xh) / math.pi
-                if yh < 0:    
-                        tiltheading += 360
-                return tiltheading
-
+        def get_tilt_teading(self):
+            time.sleep(2)
+            self.COMserial.write('y'.encode())
+            til = self.COMserial.readline()
+            return til
--- main.py
+++ main.py
@@ -1,23 +1,63 @@
 from IMU_10D0F import IMU10DOF
+from binascii import hexlify
+from binascii import unhexlify
+import serial
 import time
+
+def parse_dados(txt):
+        txt = txt.decode("utf-8")
+        inicio = 0
+        fim = 0
+        dado = []
+        for i in range(int(len(txt))):
+            if(txt[i] != ','):
+                fim += 1
+            else:
+                dado.append(txt[inicio:fim])
+                inicio = fim + 1
+                fim = fim + 1
+        dado.append(txt[inicio:(int(len(txt)))])
+
+        print("x = ", dado[0])
+        print("y = ", dado[1])
+        print("z = ", dado[2])
+
+        return dado
+
 
 if __name__ == "__main__":
 
-    reads_Robot = IMU10DOF()
+    serial = IMU10DOF('/dev/ttyACM0',115200)
+    #serial = serial.Serial('/dev/ttyACM0',115200)
+    print('oiiiii')
+    while(True):
+        #serial.write("m")
+        #x = serial.readline()
+        #y = serial.readline()
+        #z = str(hexlify(serial.read()))
+        #print(y)
 
-    print("******Magnetômetro******")
-    print(reads_Robot.read_magnetometer())
-    print("************************")
-    print()
-    print('------Acelerometro------')
-    print(reads_Robot.read_accelerometer())
-    print('------------------------')
-    print()
-    print('=======Giroscopio=======')
-    print(reads_Robot.read_gyroscope())
-    print('========================')
-    print("O ângulo no sentido horário entre o norte magnético e o eixo X:")
-    print(reads_Robot.getHeading())
-    print("O ângulo no sentido horário entre o norte magnético e a projeção do eixo X positivo no plano horizontal:")
-    print(reads_Robot.getTiltHeading())
-    time.sleep(0.5)
+        mag = serial.read_magnetometer()
+        parse_dados(mag)
+        print('----------------------------------------')
+        acc = serial.read_accelerometer()
+        parse_dados(acc)
+        print('----------------------------------------')
+        gyr = serial.read_gyroscope()
+        parse_dados(gyr)
+        print('----------------------------------------')
+        hea = serial.get_heading()
+        print(hea.decode("utf-8"))
+        print('----------------------------------------')
+        tea = serial.get_tilt_teading()
+        print(tea.decode("utf-8"))
+        print('----------------------------------------')
+        print('************************************************')
+
+        #print(parse_dados(serial.read_magnetometer()))
+        #print('------------------------------------------')
+        #print(parse_dados(serial.read_accelerometer))
+        #print('------------------------------------------')
+        #print(parse_dados(serial.read_gyroscope))
+        #print('------------------------------------------')
+        time.sleep(0.5)
